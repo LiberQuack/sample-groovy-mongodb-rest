0cf6008e7315cb21bf0d4e500ea92a61b6b4b619
unknown <rogue.thiago@gmail.com>
2015-01-21 01:46:40 -0200

    Improved DAO setup

--- src/main/java/br/com/samplegroup/dao/DAO.groovy
+++ src/main/java/br/com/samplegroup/dao/DAO.groovy
@@ -7,26 +7,22 @@ abstract class DAO {
 
     Jongo db
 
-    abstract Object find(obj)
     abstract Object find()
-    abstract Object remove(obj)
+    abstract Object find(obj)
     abstract Object save(obj)
+    abstract Object remove(obj)
 
     DAO() {
         DB mongDB
         def url = System.getenv("DB_URL")
-        def host = System.getenv("DB_HOST")
-        def port = System.getenv("DB_PORT")
-        def user = System.getenv("DB_USER")
-        def psw = System.getenv("DB_PASSWORD")
         def dbName = System.getenv("DB_NAME")
-        if (url) {
+        if (url && dbName) {
             def uri = new MongoClientURI(url)
             mongDB = new MongoClient(uri).getDB(dbName)
         } else {
-            def cred = new MongoCredential("PLAIN", user, dbName, psw.toCharArray())
-            def server = new ServerAddress(host, port)
-            mongDB = new MongoClient(server, Arrays.asList(cred))
+            def uri = new MongoClientURI("mongodb://1.0.0.7")
+            mongDB = new MongoClient(uri).getDB("todo-app")
+            System.err.print("WARNING, ENVS DB_URL/DB_NAME NOT FOUND, RUNNING AGAINST 1.0.0.7")
         }
         mongDB.setWriteConcern(WriteConcern.SAFE)
         this.db = new Jongo(mongDB)
