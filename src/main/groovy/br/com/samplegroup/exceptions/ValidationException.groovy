38577562f79de25580a5dd0bb7ba3448654a9bc9
unknown <rogue.thiago@gmail.com>
2015-03-03 01:28:35 -0300

    Added a new Todo

--- src/main/groovy/br/com/samplegroup/exceptions/ValidationException.groovy
+++ src/main/groovy/br/com/samplegroup/exceptions/ValidationException.groovy
@@ -4,6 +4,7 @@ class ValidationException extends Exception {
 
     Map fieldsAndReasons
 
+    /*TODO: Need to implement exceptions as JSON:API*/
     ValidationException(Map fieldsAndReasons, Object invalidObject) {
         super("The object type of ${invalidObject.getClass()} is invalid")
         this.fieldsAndReasons = [errs:fieldsAndReasons]
